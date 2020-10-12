084a15b9ab773e65e4d5a2fdb48881f741ed37e9
Camilla Gejl Olsen <cami32d9@stud.kea.dk>
2020-10-12 21:12:55 +0200

    Added users- and standard-presets services

---
+++ src/app/standard-presets.services.ts
@@ -0,0 +1,54 @@
+export class ProductService {
+  getStandardPresets() {
+    return [
+      {
+        name: 'This room',
+        temperature: {
+          value: 20,
+          active: false
+        },
+        humidity: {
+          value: 40,
+          active: false
+        },
+        airflow: {
+          value: 5,
+          active: false
+        }
+      },
+
+      {
+        name: 'Cold + humid',
+        temperature: {
+          value: 19,
+          active: true
+        },
+        humidity: {
+          value: 75,
+          active: true
+        },
+        airflow: {
+          value: 8,
+          active: true
+        }
+      },
+
+      {
+        name: 'Quick warm up',
+        temperature: {
+          value: 25,
+          active: true
+        },
+        humidity: {
+          value: 30,
+          active: true
+        },
+        airflow: {
+          value: 10,
+          active: true
+        }
+      }
+    ]
+      ;
+  }
+}
