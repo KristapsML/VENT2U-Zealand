391301633f4e20db214a372a17b545148ac24300
Camilla Gejl Olsen <cami32d9@stud.kea.dk>
2020-10-12 20:43:57 +0200

    Added Material Module for all Matieral imports

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+
+// This module should contain all our imports for Material modules.
+
+// Message: To start with, I have just commented out the modules that were already here -
+// only keeping them here for example of how the modules should be added.
+
+import {
+//  MatButtonModule,
+//  MatToolbarModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+//    MatButtonModule,
+//    MatToolbarModule
+  ],
+  exports: [
+//    MatButtonModule,
+//    MatToolbarModule
+  ]
+})
+export class MaterialModule {}
