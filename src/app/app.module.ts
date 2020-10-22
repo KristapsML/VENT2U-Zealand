d3f56e7fdafedc20cc96b9a9b32e2b3cb2097c33
Camilla Gejl Olsen <cami32d9@stud.kea.dk>
2020-10-22 22:00:18 +0200

    Made the 'startup' flow work with selecting spot, settings and time.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
@@ -86,7 +86,8 @@ const routes: Routes = [
     UsersService,
     StandardPresetsService
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule {
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 
 // This module should contain all our imports for Material modules.
 
-// Importing slider module to experiment with customizability
 import {MatSliderModule} from '@angular/material/slider';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
@@ -11,15 +10,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
-<<<<<<< HEAD
-
-// Message: To start with, I have just commented out the modules that were already here -
-// only keeping them here for example of how the modules should be added.
-
-// Also! Remember to import the modules like the NgModule^ above
-=======
 // Remember to import the modules like the NgModule^ above
->>>>>>> b192abe3c565e3be7131a22652703d41f49eb441
 
 @NgModule({
   imports: [
