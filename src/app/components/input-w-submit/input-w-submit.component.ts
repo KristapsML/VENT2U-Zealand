fc6c14dd68157d6f30d19f1f8f4c9207281d73ff
Kristaps Lusis <kristapsl1803@gmail.com>
2020-10-30 02:58:07 +0100

    Added Form validation

--- src/app/components/input-w-submit/input-w-submit.component.ts
+++ src/app/components/input-w-submit/input-w-submit.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-input-w-submit',
@@ -6,6 +16,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-w-submit.component.scss']
 })
 export class InputWSubmitComponent implements OnInit {
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  matcher = new MyErrorStateMatcher();
+
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.password,
+  ]);
+
+  matcher = new MyErrorStateMatcher();
 
   constructor() { }
 
