8cec48314d94683d0f33ab8cdf40a99bc7c0f762
Camilla Gejl Olsen <cami32d9@stud.kea.dk>
2020-11-13 12:50:43 +0100

    Updated StartupSelectTime to update leavingTime in User in the database

--- src/app/views/startup-select-time/startup-select-time.component.ts
+++ src/app/views/startup-select-time/startup-select-time.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {PreviousRouteService} from '../../services/previous-route.service';
+import {UsersService} from '../../services/users.service';
 
 @Component({
   selector: 'app-startup-select-time',
@@ -11,13 +12,42 @@ export class StartupSelectTimeComponent implements OnInit {
   lastDest: string;
   routerDest: string;
 
+  timeSuggestions = ['11:30', '13:00', '13:55', '14:50'];
+  users = {};
+
   constructor(
+    private usersService: UsersService,
     private previousRouteService: PreviousRouteService
   ) {
   }
 
   ngOnInit() {
-
+    this.retrieveUsers();
   }
 
+  retrieveUsers() {
+    this.usersService.getAll()
+      .subscribe(
+        data => {
+          this.users = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  updateTime(userId, time) {
+    // Updates leavingTime in User
+    this.usersService.update(userId, {leavingTime: time})
+      .subscribe(
+        response => {
+          console.log(response);
+        },
+        error => {
+          console.log(error);
+        });
+
+    console.log('updating:', userId, time);
+  }
 }
