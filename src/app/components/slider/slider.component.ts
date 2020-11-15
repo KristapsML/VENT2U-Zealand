d4afbe1c5077020b169fd760e7d8ef87a415b18e
Joachim Vadmand <joac1285@edu.easj.dk>
2020-11-15 20:00:34 +0100

    Set up current spot settings

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { UsersService } from 'app/services/users.service';
+import { SpotsService } from 'app/services/spots.service';
 
 @Component({
   selector: 'app-slider',
@@ -11,9 +12,13 @@ export class SliderComponent implements OnInit {
 
   users = {};
   settingValue: number;
+  userSetting: string;
   toggleValue: boolean;
   keepAtRoomLevel = null;
-  userSetting: string;
+
+  spots = {};
+  spotValue: number;
+  spotSetting: string;
 
   @Input() props: {
     setting: string,
@@ -28,13 +33,13 @@ export class SliderComponent implements OnInit {
     preview: boolean
   };
 
-  constructor(private usersService: UsersService) { }
+  constructor(private usersService: UsersService, private spotsService: SpotsService) { }
 
   ngOnInit() {
-      this.keepAtRoomLevel = this.props.keepAtRoomLevel;
-      console.log(this.keepAtRoomLevel);
       this.retrieveUsers();
       this.userSetting = 'user'.concat(this.props.setting);
+      this.retrieveSpots();
+      this.spotSetting = 'curr'.concat(this.props.setting);
   }
 
   retrieveUsers() {
@@ -42,10 +47,22 @@ export class SliderComponent implements OnInit {
       .subscribe(
         data => {
           this.users = data;
-
           this.settingValue = this.users[0][this.userSetting];
-
           this.toggleValue = this.users[0][this.userSetting];
+
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  retrieveSpots() {
+    this.spotsService.getAll()
+      .subscribe(
+        data => {
+          this.spots = data;
+          this.spotValue = this.spots[0][this.spotSetting];
 
           console.log(data);
         },
