f542ffd2e91d4b167efdf6411caceaec1896cf4b
Kristaps Lusis <kristapsl1803@gmail.com>
2020-11-19 03:12:17 +0100

    Post through HTTPClient - Not working yet

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
-import {StartupSelectSpotModalComponent} from '../startup-select-spot/startup-select-spot-modal/startup-select-spot-modal.component';
-import {LoginModalComponent} from './login-modal/login-modal.component';
-import {MatDialog} from '@angular/material/dialog';
+import { StartupSelectSpotModalComponent } from '../startup-select-spot/startup-select-spot-modal/startup-select-spot-modal.component';
+import { LoginModalComponent } from './login-modal/login-modal.component';
+import { MatDialog } from '@angular/material/dialog';
+import { HttpClient } from '@angular/common/http';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -24,7 +25,6 @@ export class LoginComponent implements OnInit {
     Validators.required,
     Validators.email,
   ]);
-
   hide = true;
 
   passwordFormControl = new FormControl('', [
@@ -33,13 +33,34 @@ export class LoginComponent implements OnInit {
 
   matcher = new MyErrorStateMatcher();
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, private httpClient: HttpClient) { }
+
+  formGroup = new FormControl({
+    email: this.emailFormControl,
+    password: this.passwordFormControl
+  });
 
   ngOnInit(): void {
   }
 
   openDialog() {
     this.dialog.open(LoginModalComponent);
+  }
+
+  submitForm() {
+    const formData = new FormData();
+    const email = this.formGroup.get('email').value;
+    const password = this.formGroup.get('password').value;
+    formData.append('email', email);
+    formData.append('password', password);
+
+    this.httpClient.post<any>('http://localhost:8080/api/auth/login', formData).subscribe(
+      (res) => alert(res.body),
+      (err) => alert(err.body)
+    );
+
+    // alert(`EMAIL = ${email}, PASSWORD = ${password}`)
+
   }
 
 }
